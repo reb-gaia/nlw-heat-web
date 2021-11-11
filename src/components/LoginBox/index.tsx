915d27d87c2ea65a5daaebd99c50033249380539
Rebeca Gaia <reb.gaia@gmail.com>
2021-11-11 09:10:30 -0300

    criando contexto para a autenticação do usuário via GitHub

--- src/components/LoginBox/index.tsx
+++ src/components/LoginBox/index.tsx
@@ -1,46 +1,13 @@
-import { useEffect } from 'react';
+import { useContext, useEffect } from 'react';
 import { VscGithubInverted } from 'react-icons/vsc';
-import { api } from '../../services/api';
+import { AuthContext } from '../../contexts/auth';
 
 import styles from './styles.module.scss';
 
-type AuthResponse = {
-  token: string;
-  user: {
-    id: string;
-    avatar_url: string;
-    name: string;
-    login: string;
-  }
-}
-
 export function LoginBox() {
-  const signInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=1c9a9f1a1ece6c8f02cd`
+  const { signInUrl, user } = useContext(AuthContext);
 
-  async function signIn(githubCode: string) {
-    const response = await api.post<AuthResponse>('authenticate', {
-      code: githubCode
-    })
-
-    const { token, user } = response.data;
-
-    localStorage.setItem('@dowhile:token', token);
-
-    console.log(user);
-  }
-
-  useEffect(() => {
-    const url = window.location.href;
-    const hasGitHubCode = url.includes('?code=');
-
-    if (hasGitHubCode) {
-      const [urlWithoutCode, githubCode] = url.split('?code=');
-
-      window.history.pushState({}, '', urlWithoutCode);
-
-      signIn(githubCode);
-    }
-  }, [])
+  console.log(user);
 
   return (
     <div className={styles.loginBoxWrapper}> 
---
+++ src/contexts/auth.tsx
@@ -0,0 +1,87 @@
+import { createContext, ReactNode, useContext, useEffect, useState } from "react";
+import { api } from "../services/api";
+
+type User = {
+  id: number;
+  name: string;
+  login: string;
+  avatar_url: string;
+}
+
+type AuthContextData = {
+  user: User | null;
+  signInUrl: string;
+  signOut: () => void;
+}
+
+type AuthResponse = {
+  token: string;
+  user: {
+    id: number;
+    avatar_url: string;
+    name: string;
+    login: string;
+  }
+}
+
+type AuthProvider = {
+  children: ReactNode;
+}
+
+export const AuthContext = createContext({} as AuthContextData);
+
+export function AuthProvider(props: AuthProvider) {
+  const [user, setUser] = useState<User | null>(null);
+
+  const signInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=1c9a9f1a1ece6c8f02cd`;
+
+  async function signIn(githubCode: string) {
+    const response = await api.post<AuthResponse>('authenticate', {
+      code: githubCode
+    })
+
+    const { token, user } = response.data;
+
+    localStorage.setItem('@dowhile:token', token);
+ 
+    setUser(user);
+  }
+
+  function signOut() {
+    setUser(null);
+    localStorage.removeItem('@dowhile:token');
+  }
+
+  // verificando se o usuário já efetuou o login anteriormente
+  useEffect(() => {
+    const token = localStorage.getItem('@dowhile:token');
+
+    if (token) {
+      api.defaults.headers.common.authorization = `Bearer ${token}`;
+
+      api.get('profile')
+        .then(response => {
+          console.log(response.data);
+        })
+    }
+  }, [])
+
+  useEffect(() => {
+    const url = window.location.href;
+    const hasGitHubCode = url.includes('?code=');
+
+    if (hasGitHubCode) {
+      const [urlWithoutCode, githubCode] = url.split('?code=');
+
+      window.history.pushState({}, '', urlWithoutCode);
+
+      signIn(githubCode);
+    }
+  }, [])
+  
+  return (
+    <AuthContext.Provider value={{ signInUrl, user, signOut }}>
+      {props.children}
+    </AuthContext.Provider>
+  )
+}
